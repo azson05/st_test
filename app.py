8786f860fe4823a6475b153048ce6b457506558a
azson05 <songywkd@gmail.com>
2024-05-29 16:16:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
-123
+import streamlit as st
+st.text_input("Your name", key="name")
+st.session_state.name
+
+st.image("https://docs.streamlit.io/logo.svg")
+
+st.header("OpenAI API Key를 적어주세요.")
+api = st.text_input(label, type="password")
+
+if st.button("확인"):
+  from openai import OpenAI
+
+client = OpenAI(api_key=f"{api}")
+
+st.divider()
+
+st.header("무엇이든 물어보세요.")
+prompt = st.text_input("질문?")
+if st.button("실행하기"):
+   assistant = client.beta.assistants.create(
+      instructions = "You are a helpful assistant.",
+      model='gpt-3.5-turbo'
+  )
+  thread = client.beta.threads.create(
+      messages = [
+          {"role":"user", "content":f"{prompt}"}
+      ]
+  )
+  run = client.beta.threads.runs.create(
+      thread_id=thread.id,
+      assistant_id=assistant.id,
+  )
+  st.markdown(f"응답: {prompt}")
+
+st.divider()
