2dc428f87bc451f1b8465d075548e793f3036825
azson05 <songywkd@gmail.com>
2024-05-29 16:46:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ api = st.text_input("API Key?")
 if st.button("확인"):
   from openai import OpenAI
   client = OpenAI(api_key=f"{api}")
-
+  assistant = client.beta.assistants.create(
+    instructions = "You are a helpful assistant.",
+    model='gpt-3.5-turbo'
+  )
 st.divider()
 
 st.header("무엇이든 물어보세요.")
 prompt = st.text_input("질문?")
 if st.button("실행하기"):
-   assistant = client.beta.assistants.create(
-      instructions = "You are a helpful assistant.",
-      model='gpt-3.5-turbo'
-    )
+  
 
 st.divider()
