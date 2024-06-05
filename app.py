17c8fd8c0d8854458c7c30202cc6eefdefaba680
azson05 <songywkd@gmail.com>
2024-06-05 10:56:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import openai
 
 st.image("https://docs.streamlit.io/logo.svg")
 
@@ -6,16 +7,14 @@ st.header("OpenAI API Key를 적어주세요.")
 api = st.text_input("API Key?")
 
 if st.button("확인"):
-  from openai import OpenAI
-  client = OpenAI(api_key=f"{api}")
+  openai.api_key = f"{api}"
   
 st.divider()
 
 st.header("무엇이든 물어보세요.")
 prompt = st.text_input("질문?")
 if st.button("실행하기"):
-  from openai import OpenAI
-  client = OpenAI(api_key=f"{api}")
+  openai.api_key = f"{api}"
   response = openai.Completion.create(
         engine="gpt-3.5-turbo",
         prompt=prompt,
