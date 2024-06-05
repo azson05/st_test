b29e54dc647343cd26f595a5716e658ebd1f3a11
azson05 <songywkd@gmail.com>
2024-06-05 10:37:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,7 @@ st.header("OpenAI API Key를 적어주세요.")
 api = st.text_input("API Key?")
 
 if st.button("확인"):
-  from openai import OpenAI
-  client = OpenAI(api_key=f"{api}")
-  assistant = client.beta.assistants.create(
-    instructions = "You are a helpful assistant.",
-    model='gpt-3.5-turbo'
-  )
+  
 st.divider()
 
 st.header("무엇이든 물어보세요.")
