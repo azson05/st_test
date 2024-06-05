ab65ca81281c1aff3e96f83b72f012ba32060f86
azson05 <songywkd@gmail.com>
2024-06-05 11:07:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,27 +7,36 @@ st.header("OpenAI API Key를 적어주세요.")
 api = st.text_input("API Key?")
 
 if st.button("확인"):
-  openai.api_key = f"{api}"
+  from openai import OpenAI
+  client = OpenAI(api_key=f"{api}")
+  assistant = client.beta.assistants.create(
+    instructions = "You are a helpful assistant.",
+    model='gpt-3.5-turbo'
+  )
   
 st.divider()
 
 st.header("무엇이든 물어보세요.")
 prompt = st.text_input("질문?")
 if st.button("실행하기"):
-  openai.api_key = f"{api}"
-  response = openai.Completion.create(
-        engine="gpt-3.5-turbo",
-        prompt=prompt,
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-
-  answer = response.choices[0].text.strip()
-
-  st.markdown(f"질문: {prompt}")
-  st.markdown(f"답변: {answer}")
+  from openai import OpenAI
+  client = OpenAI(api_key=f"{api}")
+  assistant = client.beta.assistants.create(
+    instructions = "You are a helpful assistant.",
+    model='gpt-3.5-turbo'
+  )
+  thread = client.beta.threads.create(
+    messages=[
+      {
+          "role":"user",
+          "content": f"{prompt}"
+      }
+    ]
+  )
+  run = client.beta.threads.runs.create(
+    thread_id=thread.id,
+    assistant_id=assistant.id
+  )
   st.markdown(f"질문: {prompt}")
 
 st.divider()
